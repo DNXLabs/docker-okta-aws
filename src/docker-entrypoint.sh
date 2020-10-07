4257381af706dbd68269ef5da726358b57784f31
Felipe Rodrigues <sohflp@gmail.com>
2020-10-07 15:02:19 +1100

    Replace awscli by docker entrypoint

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -6,7 +6,7 @@ aws configure set region "${OKTA_AWS_DEFAULT_REGION:-ap-southeast-2}"
 
 # Primary OKTA login
 java "-Duser.home=${HOME}" \
-     -classpath /usr/bin/okta-aws-cli.jar com.okta.tools.awscli \
+     -classpath /bin/okta-aws-cli.jar com.okta.tools.awscli \
      --profile "${OKTA_AWS_PROFILE:-default}" sts get-caller-identity
 
 # Assume role if specified
@@ -31,3 +31,10 @@ if [ -n "${AWS_ASSUME_ROLE_ARN:-}" ]; then
   # Print out secondary role information
   aws sts get-caller-identity
 fi
+
+echo "" >> /work/.env
+echo AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id) >> /work/.env
+echo AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key) >> /work/.env
+echo AWS_SECURITY_TOKEN=$(aws configure get aws_security_token) >> /work/.env
+echo AWS_SESSION_EXPIRATION=$(aws configure get aws_session_expiration) >> /work/.env
+echo AWS_SESSION_TOKEN=$(aws configure get aws_session_token) >> /work/.env
